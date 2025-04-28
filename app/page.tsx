58d0d32ea43295519c5a16c46d0f690874ecd09b
Shahrul Estar <75235970+shahrulestar@users.noreply.github.com>
2025-04-29 02:32:34 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -83,8 +83,7 @@ export default function Home() {
           </div>
           <div className="text-gray-600 text-sm">
             {t.footerNote}{" "}
-            <a href="https://shahrulestar.com" className="underline hover:text-black transition-colors">
-              shahrulestar.com
+            
             </a>
           </div>
         </div>
